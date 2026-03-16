e0f2ae7403ad209a8985b83c7ac9da4f19b0b1a4
Caio Govea <contato.caiogovea@gmail.com>
2026-03-16 18:25:54 -0300

    Project created

---
+++ src/test/java/com/educandoweb/ProjetoCurso/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.ProjetoCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
