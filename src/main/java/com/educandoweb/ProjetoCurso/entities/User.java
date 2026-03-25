0a02dee0633ac7ab3d998664bc176a75799c5f8e
Caio Govea <contato.caiogovea@gmail.com>
2026-03-25 17:40:27 -0300

    User Insert

--- src/main/java/com/educandoweb/ProjetoCurso/entities/User.java
+++ src/main/java/com/educandoweb/ProjetoCurso/entities/User.java
@@ -45,6 +45,14 @@ public class User implements Serializable {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getEmail() {
         return email;
     }
