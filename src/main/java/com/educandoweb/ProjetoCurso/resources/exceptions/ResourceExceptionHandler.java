9e34761a3fff1587bd6462f2d52c9725346c39f2
Caio Govea <contato.caiogovea@gmail.com>
2026-03-26 11:53:09 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/ProjetoCurso/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/ProjetoCurso/resources/exceptions/ResourceExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.educandoweb.ProjetoCurso.resources.exceptions;
 
+import com.educandoweb.ProjetoCurso.services.exceptions.DataBaseException;
 import com.educandoweb.ProjetoCurso.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.Instant;
 
@@ -16,6 +18,15 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         String error = "Resource Not Found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> database (DataBaseException e, HttpServletRequest request) {
+        String error = "Database Error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
--- src/main/java/com/educandoweb/ProjetoCurso/services/UserService.java
+++ src/main/java/com/educandoweb/ProjetoCurso/services/UserService.java
@@ -2,8 +2,11 @@ package com.educandoweb.ProjetoCurso.services;
 
 import com.educandoweb.ProjetoCurso.entities.User;
 import com.educandoweb.ProjetoCurso.repositories.UserRepository;
+import com.educandoweb.ProjetoCurso.services.exceptions.DataBaseException;
 import com.educandoweb.ProjetoCurso.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,15 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DataBaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
